6bd0dfc1512805b26e61e666c6d0ebe2d90d8741
Nahom Mehanzel <nknahom@gmail.com>
2021-07-22 00:29:19 +0000

    countLetters function added

---
+++ countLetters.js
@@ -0,0 +1,22 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`\u2705 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`\u26D4 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function (sentence) {
+  let result = {};
+  let letters = sentence.replace(/\s/g, "");
+  for (let letter of letters) {
+    if (result[letter]) {
+      result[letter] += 1;
+    } else {
+      result[letter] = 1;
+    }
+  }
+  return result;
+};
+
+console.log(countLetters("lighthouse in the house"));
