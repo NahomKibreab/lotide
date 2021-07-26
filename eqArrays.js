ec20e20076b89dcb8032ec4f7613f605ccb98635
Nahom Mehanzel <nknahom@gmail.com>
2021-07-26 20:22:49 +0000

    refactor eqArrays and move test code to eqArraysTest inside test folder

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,3 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`\u2705 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`\u26D4 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const eqArrays = function (array1, array2) {
   let isEqual = true;
   if (array1.length === array2.length) {
@@ -20,4 +12,4 @@ const eqArrays = function (array1, array2) {
   return isEqual;
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+module.exports = eqArrays;
