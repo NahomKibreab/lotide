eca181c67efda43b384ebd11019f596e673810de
Nahom Mehanzel <nknahom@gmail.com>
2021-07-26 20:44:24 +0000

    refactor middle and seperate the test code to middleTest file inside test folder

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -2,9 +2,9 @@ const eqArrays = require("./eqArrays");
 
 const assertArraysEqual = function (array1, array2) {
   if (eqArrays(array1, array2)) {
-    console.log(`\u2705 Assertion Passed: ${array1} === ${array2}`);
+    console.log(`\u2705 Assertion Passed: [${array1}] === [${array2}]`);
   } else {
-    console.log(`\u26D4 Assertion Failed: ${array1} !== ${array2}`);
+    console.log(`\u26D4 Assertion Failed: [${array1}] !== [${array2}]`);
   }
 };
 
--- middle.js
+++ middle.js
@@ -1,26 +1,3 @@
-const eqArrays = function (array1, array2) {
-  let isEqual = false;
-  if (array1.length === array2.length) {
-    array1.forEach((element, index) => {
-      console.log("array2 with index: ", array2[index]);
-      if (element !== array2[index]) {
-        isEqual = false;
-      } else {
-        isEqual = true;
-      }
-    });
-  }
-  return isEqual;
-};
-
-const assertArraysEqual = function (array1, array2) {
-  if (eqArrays(array1, array2)) {
-    console.log(`\u2705 Assertion Passed: ${array1} === ${array2}`);
-  } else {
-    console.log(`\u26D4 Assertion Failed: ${array1} !== ${array2}`);
-  }
-};
-
 const middle = function (array) {
   if (array.length === 1 || array.length === 2) {
     return [];
@@ -33,8 +10,4 @@ const middle = function (array) {
   }
 };
 
-console.log("[]", middle([]));
-console.log("[1]", middle([1]));
-console.log("[1,2]", middle([1, 2]));
-console.log("[1,2,3]", middle([1, 2, 3]));
-console.log("[1,2,3,4]", middle([1, 2, 3, 4]));
+module.exports = middle;
